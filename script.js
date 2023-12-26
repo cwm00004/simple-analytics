7d8e57067638cde5cc93f1a551a6539bf1c91fb2
CWM00004 <cwm00004@mix.wvu.edu>
2023-12-26 14:07:49 -0500

    fixing ai garbo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('loginWithSpotify').addEventListener('click', function() {
+
     // Spotify authentication URL
     const clientId = 'b5a00bbb23724ed3ab30250d5f01fc71'; // get from spotify dev dashbaord
     const redirectUri = encodeURIComponent('https://cwm00004.github.io/simple-analytics/Dashboard.html'); // get from github pages hosting of website
@@ -6,4 +6,3 @@ document.getElementById('loginWithSpotify').addEventListener('click', function()
 
     // Redirect the user to Spotify's authentication page
     window.location.href = spotifyAuthUrl;
-});
