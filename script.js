6165345b18980fc8b2e677329b85d18c6ef7acd0
CWM00004 <cwm00004@mix.wvu.edu>
2023-12-26 14:10:54 -0500

    zdg

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
     // Spotify authentication URL
     const clientId = 'b5a00bbb23724ed3ab30250d5f01fc71'; // get from spotify dev dashbaord
     const redirectUri = encodeURIComponent('https://cwm00004.github.io/simple-analytics/Dashboard.html'); // get from github pages hosting of website
